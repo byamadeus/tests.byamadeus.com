29561ce4ca83a66d4be0ba3a5a8758f69c748b72
byamadeus <evanamadeuscameron@gmail.com>
2026-02-16 16:49:35 -0500

    Update web-components.d.ts

--- types/web-components.d.ts
+++ types/web-components.d.ts
@@ -1,8 +1,13 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'portfolio-nav': React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    >;
+// types/web-components.d.ts
+import React from 'react';
+
+declare global {
+  namespace React.JSX {
+    interface IntrinsicElements {
+      'portfolio-nav': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
   }
 }
